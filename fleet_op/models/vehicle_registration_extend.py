de67363c756ff0b341ca8719df5196e6f7691853
shivamrajput21 <shivraj8077@gmail.com>
2020-03-02 17:20:57 +0530

    [ADD/IMP][add fleet operation reports]

--- fleet_op/models/vehicle_registration_extend.py
+++ fleet_op/models/vehicle_registration_extend.py
@@ -160,3 +160,122 @@ class VehicleType(models.Model):
     name = fields.Char(string="name")
 
 
+
+
+class FleetVehicleAdvanceSearch(models.TransientModel):
+    """Model fleet vehicle advance search."""
+
+    _name = 'fleet.vehicle.advance.search'
+    _description = 'Vehicle Advance Search'
+    _rec_name = 'fmp_id'
+
+    fmp_id = fields.Many2one('fleet.vehicle', string="Vehicle ID")
+    vehicle_location_id = fields.Many2one('res.country.state',
+                                          string='Province')
+    state = fields.Selection([('inspection', 'Inspection'),
+                              ('in_progress', 'In Progress'),
+                              ('complete', 'Completed'),
+                              ('released', 'Released'),
+                              ('write-off', 'Write-Off')], string='Status')
+    vehical_color_id = fields.Many2one('color.color', string='Color')
+    vin_no = fields.Char(string='Vin No', size=64)
+    engine_no = fields.Char(string='Engine No', size=64)
+    last_service_date = fields.Date(string='Last Service From')
+    last_service_date_to = fields.Date(string='Last Service To')
+    next_service_date = fields.Date(string='Next Service From')
+    next_service_date_to = fields.Date(string='Next Service To')
+    acquisition_date = fields.Date(string="Registration From")
+    acquisition_date_to = fields.Date(string="Registration To")
+    release_date_from = fields.Date(string='Released From')
+    release_date_to = fields.Date(string='Released To')
+    driver_identification_no = fields.Char(string='Driver ID', size=64)
+    vechical_type_id = fields.Many2one('vehicle.type', string='Vechical Type')
+    division_id = fields.Many2one('vehicle.divison', string="Division")
+    make_id = fields.Many2one("fleet.vehicle.model.brand", string="Make")
+    model_id = fields.Many2one("fleet.vehicle.model", string="Model")
+
+
+
+class FleetWorkOrderSearch(models.TransientModel):
+    """Fleet Workorder search model."""
+
+    _name = 'fleet.work.order.search'
+    _description = 'Fleet Workorder Search'
+    _rec_name = 'state'
+
+    priority = fields.Selection([('normal', 'NORMAL'), ('high', 'HIGH'),
+                                 ('low', 'LOW')], string='Order Priority')
+    state = fields.Selection([('confirm', 'Open'), ('done', 'Close'),
+                              ('any', 'Any')], string='Status')
+    part_id = fields.Many2one('product.product', string='Parts')
+    issue_date_from = fields.Date(string='Issue From')
+    issue_date_to = fields.Date(string='Issue To')
+    open_date_from = fields.Date(string='Open From')
+    open_date_to = fields.Date(string='Open To')
+    close_date_form = fields.Date(string='Close From')
+    close_date_to = fields.Date(string='Close To')
+    vehical_division_id = fields.Many2one('vehicle.divison',
+                                          string="Division")
+    work_order_id = fields.Many2one('fleet.vehicle.log.services',
+                                    string='Service Order')
+    fmp_id = fields.Many2one('fleet.vehicle', string='Vehicle ID')
+    cost_subtype_id = fields.Many2one('fleet.service.type',
+                                      string='Service Type')
+    repair_type_id = fields.Many2one('repair.type', string='Repair Type')
+    # open_days = fields.Char(string='Open Days', size=16)
+    make_id = fields.Many2one("fleet.vehicle.model.brand", string="Make")
+    model_id = fields.Many2one("fleet.vehicle.model", string="Model")
+
+
+class FleetWittenOff(models.Model):
+    """Model Fleet Witten Off."""
+
+    _name = 'fleet.wittenoff'
+    _description = 'Wittenoff Vehicles'
+    _order = 'id desc'
+    _rec_name = 'vehicle_id'
+
+    name = fields.Char(string="Name")
+    fleet_id = fields.Integer(string='Fleet ID',
+                              help="Take this field for data migration")
+    vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle',
+                                 required=True)
+    vehicle_fmp_id = fields.Char(string='Vehicle ID', size=64)
+    vin_no = fields.Char(string='Vin No', size=64, translate=True)
+    color_id = fields.Many2one('color.color', string='Color')
+    vehicle_plate = fields.Char(string='Vechicle Plate No.', translate=True)
+    report_date = fields.Date(string='Report Date')
+    odometer = fields.Float(string='Odometer')
+    cost_esitmation = fields.Float(string='Cost Estimation')
+    note_for_cause_damage = fields.Text(string='Cuause of Damage',
+                                        translate=True)
+    note = fields.Text(string='Note', translate=True)
+    cancel_note = fields.Text(string='Cancel Note', translate=True)
+    multi_images = fields.Many2many('ir.attachment',
+                                    'fleet_written_off_attachment_rel',
+                                    'writeoff_id',
+                                    'attachment_id', string='Multi Images')
+    damage_type_ids = fields.Many2many('damage.types', 'fleet_wittenoff_damage_types_rel',
+                                       'write_off_id', 'damage_id', string="Damage Type")
+    repair_type_ids = fields.Many2many('repair.type', 'fleet_wittenoff_repair_types_rel',
+                                       'write_off_id', 'repair_id', string="Repair Type")
+    location_id = fields.Many2one('vehicle.location', string='Location')
+    driver_id = fields.Many2one('res.partner', string='Driver')
+    write_off_type = fields.Selection([
+        ('general_accident', 'General Accident'),
+        ('insurgent_attack', 'Insurgent Attack')],
+        string='Write-off Type', default='general_accident')
+    contact_no = fields.Char(string='Driver Contact Number')
+    odometer_unit = fields.Selection([('kilometers', 'Kilometers'),
+                                      ('miles', 'Miles')],
+                                     string='Odometer Unit',
+                                     help='Unit of the odometer ')
+    province_id = fields.Many2one('res.country.state', 'Registration State')
+    division_id = fields.Many2one('vehicle.divison', 'Division')
+    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'),
+                              ('cancel', 'Cancelled')],
+                             string='State', default='draft')
+    date_cancel = fields.Date(string='Date Cancelled')
+    cancel_by_id = fields.Many2one('res.users', string="Cancelled By")
+
+
